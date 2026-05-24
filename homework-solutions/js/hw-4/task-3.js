89028cf0d05ce884cb9e31ad46a501080f624671
Evgeni Samoilenko <evgeni.samoilenka@gmail.com>
2026-05-24 23:12:33 +0300

    Added homework 4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,20 @@
   преобразовываясь в number
 
 */
+
+let minAge = 18;
+let maxAge = 60;
+let age = "10";
+age = +age;
+
+if (typeof age !== "number") {
+  console.log("Incorrect data type");
+} else if (age < minAge) {
+  console.log(`You don't have access because your age is ${age}. It's less then ${minAge}`);
+} else if (age >= minAge && age <= maxAge ) {
+  console.log("Welcome!");
+} else if (age > maxAge ) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
