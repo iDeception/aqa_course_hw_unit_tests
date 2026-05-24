fe3ef78343aaffc34e8e7a9059c44e1fac099b9b
Evgeni Samoilenko <evgeni.samoilenka@gmail.com>
2026-05-25 00:21:11 +0300

    Updated hw-4

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -4,15 +4,15 @@
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
 
-  let salary = 999;
-  let grade;
+let salary = 1000;
+let grade;
 
-  if(salary >= 1000) {
-    grade = "middle";
-  } else {
-    grade = "junior"
-  }
+if (salary >= 1000) {
+  grade = 'middle';
+} else {
+  grade = 'junior';
+}
 console.log(grade);
 
-grade = (salary >= 1000) ? "middle" : "junior";
+grade = salary >= 1000 ? 'middle' : 'junior';
 console.log(grade);
--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -15,21 +15,20 @@
 
   */
 
-  let minAge = 18;
+let minAge = 18;
+let maxAge = 60;
+let age = 18;
 
-  let maxAge = 60;
-
-  let age;
-
-
-if (typeof age !== "number") {
-  console.log("Incorrect data type");
-} else if (age < minAge) {
-  console.log(`You don't have access because your age is ${age}. It's less then ${minAge}`);
-} else if (age >= minAge && age <= maxAge ) {
-  console.log("Welcome!");
-} else if (age > maxAge ) {
-  console.log("Keep calm and look Culture channel");
+if (typeof age === 'number') {
+  if (age < minAge) {
+    console.log(`You don't have access because your age is ${age}. It's less then ${minAge}`);
+  } else if (age >= minAge && age <= maxAge) {
+    console.log('Welcome!');
+  } else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log('Technical work');
+  }
 } else {
-  console.log("Technical work");
+  console.log('Incorrect data type');
 }
